335747d1f15d6ce52c5be7c3c64f98bf35144c30
Arif Budiman <arifb5624@gmail.com>
2026-09-06 13:49:50 +0700

    feat(tv): domain entities (Tv, TvDetail, Season) + interface TvRepository

---
+++ lib/domain/entities/tv_detail.dart
@@ -0,0 +1,55 @@
+import 'package:ditonton/domain/entities/genre.dart';
+import 'package:ditonton/domain/entities/season.dart';
+import 'package:equatable/equatable.dart';
+
+class TvDetail extends Equatable {
+  TvDetail({
+    required this.adult,
+    required this.backdropPath,
+    required this.genres,
+    required this.id,
+    required this.originalName,
+    required this.overview,
+    required this.posterPath,
+    required this.firstAirDate,
+    required this.name,
+    required this.numberOfSeasons,
+    required this.numberOfEpisodes,
+    required this.seasons,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final bool adult;
+  final String? backdropPath;
+  final List<Genre> genres;
+  final int id;
+  final String originalName;
+  final String overview;
+  final String posterPath;
+  final String firstAirDate;
+  final String name;
+  final int numberOfSeasons;
+  final int numberOfEpisodes;
+  final List<Season> seasons;
+  final double voteAverage;
+  final int voteCount;
+
+  @override
+  List<Object?> get props => [
+        adult,
+        backdropPath,
+        genres,
+        id,
+        originalName,
+        overview,
+        posterPath,
+        firstAirDate,
+        name,
+        numberOfSeasons,
+        numberOfEpisodes,
+        seasons,
+        voteAverage,
+        voteCount,
+      ];
+}
